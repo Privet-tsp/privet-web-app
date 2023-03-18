d2b1f8076ca884a6053f4fd068529d5c4f2d736c
katyapank <ccoo00@yandex.ru>
2023-03-18 18:55:15 +0400

    initial commit

---
+++ index.js
@@ -0,0 +1,54 @@
+import express from "express";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import mongoose from "mongoose";
+import { validationResult } from "express-validator";
+import UserModel from "./models/User.js";
+
+import { registerValidation } from "./validations/auth.js";
+
+mongoose
+    .connect(
+        "mongodb+srv://admin:wwwwww@cluster0.am9ssuk.mongodb.net/privet?retryWrites=true&w=majority"
+    )
+    .then(() => console.log("DB ok"))
+    .catch((err) => console.log("DB error", err));
+const app = express();
+
+app.use(express.json());
+
+app.post("/auth/register", registerValidation, async (req, res) => {
+    try {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json(errors.array());
+        }
+
+        const password = req.body.password;
+        const salt = await bcrypt.genSalt(10);
+        const passwordHash = await bcrypt.hash(password, salt);
+
+        const doc = new UserModel({
+            email: req.body.email,
+            userName: req.body.userName,
+            avatarUrl: req.body.avatarUrl,
+            passwordHash,
+        });
+
+        const user = await doc.save();
+
+        res.json(user);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            message: "Ошибка регистрации",
+        });
+    }
+});
+
+app.listen(4444, (err) => {
+    if (err) {
+        return console.log(err);
+    }
+    console.log("Server OK");
+});
---
+++ models/user.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+    {
+        userName: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        passwordHash: {
+            type: String,
+            required: true,
+        },
+        avatarUrl: {
+            type: String,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+export default mongoose.model("User", UserSchema, "users");
